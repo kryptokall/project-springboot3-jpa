584714bd3a423a18fc22a2970cc2bcd181705176
kryptokall <lleu.marques29@gmail.com>
2025-01-17 17:48:08 -0300

    Service layer, component registration

--- src/main/java/com/estudos/course/repositories/UserRepository.java
+++ src/main/java/com/estudos/course/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.estudos.course.entities.User;
 
+// Não é necessária a annotation @Repository, pois a classe herda de JpaRepository, que já é registrada como repository do Spring
 public interface UserRepository extends JpaRepository<User, Long> {
 	
 }
